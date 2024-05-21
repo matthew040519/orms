9c1085d228214d215dc3e4d7105dc95d2844109a
matthew040519 <matthewblancada25@gmail.com>
2024-05-21 22:30:00 +0800

    put prompt message

--- clients/index.php
+++ clients/index.php
@@ -66,7 +66,7 @@
         <!-- [ breadcrumb ] end -->
         <!-- [ Main Content ] start -->
         <div class="row">
-            
+        <button type="button" style="display: none;" id="modal" class="btn  btn-primary" data-toggle="modal" data-target="#exampleModalCenter"><i class="feather icon-plus"></i> Add Rooms</button>
             <table id="example" class="table table-striped" style="width:100%">
                 <thead>
                     <tr>
@@ -120,101 +120,27 @@
     </div>
 </div>
                         <div id="exampleModalCenter" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-							<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+							<div class="modal-dialog modal-dialog-centered modal-md" role="document">
 								<div class="modal-content">
 									<div class="modal-header">
-										<h5 class="modal-title" id="exampleModalCenterTitle">Add Rooms</h5>
+										<h5 class="modal-title" id="exampleModalCenterTitle">Instructions for Booking:</h5>
 										<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 									</div>
 									<div class="modal-body">
-                                        <form method="POST" enctype="multipart/form-data">
-											<div class="form-group">
-												<label for="recipient-name" class="col-form-label">Room Image:</label>
-												<input type="file" required class="form-control" name="image" id="room-image">
-											</div>
-                                            <div class="form-group">
-												<label for="message-text" class="col-form-label">Room Name</label>
-												<input type="text" required name="room_name" class="form-control">
-											</div>
-                                            <div class="row">
-                                                <div class="col-md-4">
-                                                    <div class="form-group">
-                                                        <label for="message-text" class="col-form-label">Good for # of Person:</label>
-                                                        <input type="number" required name="good_for_person" class="form-control">
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-4">
-                                                    <div class="form-group">
-                                                        <label for="message-text" class="col-form-label">Bedroom</label>
-                                                        <input type="number" required name="bedroom" class="form-control">
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-4">
-                                                    <div class="form-group">
-                                                        <label for="message-text" class="col-form-label">Bath</label>
-                                                        <input type="number" required name="bath" class="form-control">
-                                                    </div>
-                                                </div>
-                                            </div>
-											<div class="row">
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label for="message-text" class="col-form-label">Floor Area</label>
-                                                        <input type="number" required name="floor_area" class="form-control">
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label for="message-text" class="col-form-label">Rate</label>
-                                                        <input type="text" required name="rate" class="form-control">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-												<label for="message-text" class="col-form-label">Room Details</label>
-												<textarea name="room_details" required class="form-control" id="" cols="30" rows="10"></textarea>
-											</div>
-										
+                                        <ol>
+                                            <li class="mb-3">Select Your Start Date: Slide to the specific date you wish to begin your booking.</li>
+                                            <li class="mb-3">Choose Your End Date: Continue sliding until you reach the end date you desire for your booking.</li>
+                                            <li class="mb-3">Confirm Your Selection: Once you've set both the start and end dates, finalize your booking by confirming your selection.</li>
+                                        </ol>
                                     </div>
 									<div class="modal-footer">
 										<button type="button" class="btn  btn-secondary" data-dismiss="modal">Close</button>
-										<button type="submit" name="submit" class="btn  btn-primary">Save Room</button>
+										<!-- <button type="submit" name="submit" class="btn  btn-primary">Save Room</button> -->
 									</div>
                                     </form>
 								</div>
 							</div>
 						</div>
-
-
-        <?php
-        
-                if(isset($_POST['submit'])){
-
-                    $target = "../images/". basename($_FILES['image']['name']);
-                    $image = $_FILES['image']['name'];
-                    $room_name = $_POST['room_name'];
-                    $good_for_person = $_POST['good_for_person'];
-                    $bedroom = $_POST['bedroom'];
-                    $bath = $_POST['bath'];
-                    $floor_area = $_POST['floor_area'];
-                    $rate = $_POST['rate'];
-                    $room_detail = $_POST['room_details'];
-
-                    if (move_uploaded_file($_FILES['image']['tmp_name'], $target)){
-
-                        $query = mysqli_query($con, "INSERT INTO rooms (`room_name`, `good_for`, `Image`, `details`, `Bedroom`, `Baths`, `floor_area`, `Rate`) 
-                        Values ('$room_name', '$good_for_person', '$image','$room_detail', '$bedroom', '$bath', '$floor_area', '$rate')");
-
-                        echo "<script>alert('Successfully Upload')</script>";
-                        echo "<script>window.location.replace('rooms.php')</script>";
-                      } else {
-                        echo "<script>alert('Error!')</script>";
-                    }
-
-                   
-                }
-        
-        ?>
 
     <!-- Required Js -->
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
@@ -232,6 +158,9 @@
 <!-- custom-chart js -->
 <script src="../assets/js/pages/dashboard-main.js"></script>
 <script>
+    $(document).ready(function (){
+        $('#modal').click();
+    });
     new DataTable('#example');
 </script>
 </body>
